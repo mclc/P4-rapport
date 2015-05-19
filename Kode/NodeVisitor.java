cd544a2303403484061cd7b3086c0304fc784a6e
Mikkel Christensen <mikkel@mclc.dk>
2015-05-19 13:38:15 +0200

    Updates from ShareLaTeX

---
+++ Kode/NodeVisitor.java
@@ -0,0 +1,17 @@
+public final class Start extends Node
+{
+    @Override
+    public void apply(Switch sw)
+    {
+        ((Analysis) sw).caseStart(this);
+    }
+}
+
+public final class AProgram extends PProgram
+{
+    @Override
+    public void apply(Switch sw)
+    {
+        ((Analysis) sw).caseAProgram(this);
+    }
+}
