0e359c04634bce6b68a37d8ae22bf96cdf27c083
Mikkel Christensen <mikkel@mclc.dk>
2015-05-24 08:27:57 +0200

    Updates from ShareLaTeX

--- Kode/ExprTypeEvaluator.java
+++ Kode/ExprTypeEvaluator.java
@@ -1,33 +1,33 @@
 public class ExpressionTypeEvaluator extends DepthFirstAdapter {
     private Stack<SymbolType> stack;
     
-    // Constants
-    @Override
     public void outAIntegerExpr(AIntegerExpr node) {
         super.outAIntegerExpr(node);
         stack.push(SymbolType.Int());
     }
 
-    @Override
-    public void outADecimalExpr(ADecimalExpr node) {
-        super.outADecimalExpr(node);
-        stack.push(SymbolType.Decimal());
-    }
-
-    @Override
     public void outATrueExpr(ATrueExpr node) {
         super.outATrueExpr(node);
         stack.push(SymbolType.Boolean());
     }
 
-    @Override
     public void outACompareGreaterExpr(ACompareGreaterExpr node) {
         super.outACompareGreaterExpr(node);
+        checkComparison(node, node.getLeft(), node.getRight());
+    }
+    
+    private void checkComparison(Node node, PExpr left, PExpr right) {
         SymbolType arg2 = stack.pop(), arg1 = stack.pop();
 
         if (arg1.equals(SymbolType.Type.Int) && arg2.equals(SymbolType.Type.Int)) {
             stack.push(SymbolType.Boolean());
-        } else if //...
+        } // ...
+        } else if (arg1.equals(SymbolType.Type.Decimal) && arg2.equals(SymbolType.Type.Int)) {
+            // Promote right
+            right.replaceBy(new ATypeCastExpr(new ADoubleTypeSpecifier(), (PExpr) right.clone())); // ...
+        } else {
+            throw new IncompaitbleTypesException(node, arg1, arg2);
+        }
     }
 
 }
--- Kode/Stack.java
+++ Kode/Stack.java
@@ -1,30 +1,22 @@
-public class Stack{
-
-  // 
-
-  public void push(int value){
+public < T > void push(T value)
+{
+  if(stackPointer <= 510){
     stackPointerIncrement();
-    generator.Emit("MOVL &" + value + " " + stackPointer(), true);
-  }
-  
-  public void push(float value){
-    stackPointerIncrement();
-    generator.Emit("+F +0,0 +" + value.toString().replace(".", ",") + " " + stackPointer() + "", true);
-  }
-  
-  public void push(String value){
-    stackPointerIncrement();
-    generator.Emit("MOVL " + value + " " + stackPointer(), true);
-  }
-  
-  public void push(Boolean value){
-    stackPointerIncrement();
-    if ((Boolean)value)
-      generator.Emit("SET " + stackPointer() + ".00", true);
-    else
-      generator.Emit("RSET " + stackPointer() + ".00", true);
-  }
-  
-  //
-  
+    if (value.getClass() == Integer.class)
+      generator.Emit("MOVL(498) &" + value + " " + stackPointer(), true);
+    else if (value.getClass() == Float.class)
+      generator.Emit("+F(454) +0,0 +" + value.toString().replace(".", ",") + " " + stackPointer() + "", true);
+    else if (value.getClass() == String.class)
+      generator.Emit("MOVL(498) " + value + " " + stackPointer(), true);
+    else if (value.getClass() == Boolean.class){
+      if ((Boolean)value)
+        generator.Emit("SET " + stackPointer() + ".00", true);
+      else
+        generator.Emit("RSET " + stackPointer() + ".00", true);
+    }
+  else
+    throw new NotImplementedException();
+  } 
+  else
+    throw new OutOfMemoryError();
 }
