58512bdcb40bf38233429d42e9f7f1b7f755e14a
Mikkel Christensen <mikkel@mclc.dk>
2015-05-26 22:14:21 +0200

    Updates from ShareLaTeX

--- Kode/NodeVisitor.java
+++ Kode/NodeVisitor.java
@@ -1,17 +1,17 @@
 public final class Start extends Node
 {
-    @Override
-    public void apply(Switch sw)
-    {
-        ((Analysis) sw).caseStart(this);
-    }
+  @Override
+  public void apply(Switch sw)
+  {
+    ((Analysis) sw).caseStart(this);
+  }
 }
 
 public final class AProgram extends PProgram
 {
-    @Override
-    public void apply(Switch sw)
-    {
-        ((Analysis) sw).caseAProgram(this);
-    }
+  @Override
+  public void apply(Switch sw)
+  {
+    ((Analysis) sw).caseAProgram(this);
+  }
 }
